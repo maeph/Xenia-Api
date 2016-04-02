27231c3984321b9cd6ccf024c4b712b03b6df4fb
Lukasz Sawicki <sawickil@gmail.com>
2016-04-02 15:01:48 +0200

    [Eksport wyników losowania] Updated export events test

--- src/test/groovy/pl/jug/torun/xenia/rest/ExportEventServiceTest.groovy
+++ src/test/groovy/pl/jug/torun/xenia/rest/ExportEventServiceTest.groovy
@@ -1,44 +1,43 @@
 package pl.jug.torun.xenia.rest
 
-import org.junit.Test
-import org.mockito.Mock
 import pl.jug.torun.xenia.dao.EventRepository
 import pl.jug.torun.xenia.dao.PrizeRepository
 import pl.jug.torun.xenia.model.Event
 import pl.jug.torun.xenia.model.Prize
 import spock.lang.Specification
 
-import static org.assertj.core.api.Assertions.assertThat
-import static org.mockito.Mockito.mock
-import static org.mockito.Mockito.when
-
 /**
  * Created by Lukasz on 2016-04-02.
  */
-class ExportEventServiceTest extends Specification{
+class ExportEventServiceTest extends Specification {
 
-    EventRepository eventRepository = mock(EventRepository)
-    PrizeRepository prizeRepository = mock(PrizeRepository)
-
+    EventRepository eventRepository = Mock(EventRepository)
+    PrizeRepository prizeRepository = Mock(PrizeRepository)
 
     def "should return events"() {
 
         given:
-        def exportEventService = new ExportEventService(eventRepository, prizeRepository)
-        when(eventRepository.findAll()).thenReturn([
-                new Event(meetupId: 1234, title: "JUG Meeting"),
-                new Event(meetupId: 2343, title: "JUG Get Source")
-        ])
-        when(prizeRepository.findAll()).thenReturn([
-                new Prize(name: "Ja", producer: "Cos", id: 123, sponsorName: "Sponsor", imageUrl: "url")
-        ])
-
+            def exportEventService = new ExportEventService(eventRepository, prizeRepository)
+            eventRepository.findAll() >> [
+                    new Event(meetupId: 1234, title: "JUG Meeting"),
+                    new Event(meetupId: 2343, title: "JUG Get Source")
+            ]
+            prizeRepository.findAll() >> [
+                    new Prize(name: "Sex for Dummies", producer: "Bookmann", id: 123, sponsorName: "JUG Torun", imageUrl: "http://sex4dummies.com/img")
+            ]
 
         when:
-        def expected = exportEventService.exportAllEvents()
+            def expected = exportEventService.exportAllEvents()
 
         then:
             expected
-        expected.prizes.size()==1
+            expected.prizes.size() == 1
+
+            def bookPrize = expected.prizes[0]
+            bookPrize.id == "123"
+            bookPrize.name == "Sex for Dummies"
+            bookPrize.producer == "Bookmann"
+            bookPrize.sponsorName == "JUG Torun"
+            bookPrize.imageUrl == "http://sex4dummies.com/img"
     }
 }
