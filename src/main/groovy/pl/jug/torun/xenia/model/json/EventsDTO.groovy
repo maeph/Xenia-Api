e5688a87d343cfda4b66fa5c1a823b545f01d69a
Lukasz <lukasz.ogan@gmail.com>
2016-04-02 14:32:44 +0200

    export service

--- src/main/groovy/pl/jug/torun/xenia/model/json/EventsDTO.groovy
+++ src/main/groovy/pl/jug/torun/xenia/model/json/EventsDTO.groovy
@@ -3,4 +3,8 @@ package pl.jug.torun.xenia.model.json
 class EventsDTO {
     List<PrizeDTO> prizes
     List<EventDTO> events
+
+    EventsDTO(List<PrizeDTO> prizes) {
+        this.prizes = prizes
+    }
 }
