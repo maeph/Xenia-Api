d4a674b9d4a8eaeb0a5d1359733f451ab7c7b19a
phiotr <skoczek192@gmail.com>
2016-04-02 14:21:42 +0200

    ImportEvents controller and service

---
+++ src/main/groovy/pl/jug/torun/xenia/rest/ImportEventsController.groovy
@@ -0,0 +1,26 @@
+package pl.jug.torun.xenia.rest
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RestController
+import pl.jug.torun.xenia.model.json.EventDTO
+import pl.jug.torun.xenia.model.json.EventsDTO
+import pl.jug.torun.xenia.model.json.PrizeDTO
+
+/**
+ * Created by piotr on 02.04.16.
+ */
+@RestController
+@RequestMapping("/event/import")
+class ImportEventsController {
+
+    @Autowired
+    ImportEventsService importService
+
+    @RequestMapping(method = RequestMethod.POST, consumes = ["application/json"])
+    void putEvents(@RequestBody EventsDTO request) {
+        importService.importEvents(request)
+    }
+}
