bc12674857c908945e551df423b795dcb4591f0f
Lukasz Sawicki <sawickil@gmail.com>
2016-04-04 16:13:57 +0200

    [Import events] Fixed compilation error due to failing test
    Also removed mockito
    
    Note: some earlier change for Prize (string id) broke the integration test. Fix it!

--- src/main/groovy/pl/jug/torun/xenia/rest/ImportEventsService.groovy
+++ src/main/groovy/pl/jug/torun/xenia/rest/ImportEventsService.groovy
@@ -1,13 +1,9 @@
 package pl.jug.torun.xenia.rest
-
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import pl.jug.torun.xenia.dao.PrizeRepository
 import pl.jug.torun.xenia.model.Prize
-import pl.jug.torun.xenia.model.json.EventDTO
 import pl.jug.torun.xenia.model.json.EventsDTO
-import pl.jug.torun.xenia.model.json.PrizeDTO
-
 /**
  * Created by piotr on 02.04.16.
  */
@@ -18,13 +14,9 @@ class ImportEventsService {
     PrizeRepository prizeRepository
 
     void importEvents(EventsDTO eventsDTO) {
-
-        def prizes = prizeRepository.findAll()
         prizeRepository.save(eventsDTO.prizes.collect {
             new Prize(id: it.id, name: it.name, producer: it.producer, sponsorName: it.sponsorName, imageUrl: it.imageUrl)
         })
     }
-
-
 
 }
