50ded64f57f9733ce49fe051bf30491f97f3ba56
Wojciech Jaworski <jaworski.wojciech@gmail.com>
2016-05-14 19:35:15 +0200

    Export events integration test

---
+++ src/test/groovy/pl/jug/torun/xenia/IntegrationSpecification.groovy
@@ -0,0 +1,40 @@
+package pl.jug.torun.xenia
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.IntegrationTest
+import org.springframework.boot.test.SpringApplicationContextLoader
+import org.springframework.test.context.ContextConfiguration
+import pl.jug.torun.xenia.dao.DrawRepository
+import pl.jug.torun.xenia.dao.EventRepository
+import pl.jug.torun.xenia.dao.MeetupMemberRepository
+import pl.jug.torun.xenia.dao.MemberRepository
+import pl.jug.torun.xenia.dao.PrizeRepository
+import spock.lang.Specification
+
+@ContextConfiguration(loader = SpringApplicationContextLoader, classes = Application)
+@IntegrationTest
+class IntegrationSpecification extends Specification {
+
+    @Autowired
+    MemberRepository memberRepository
+
+    @Autowired
+    MeetupMemberRepository meetupMemberRepository
+
+    @Autowired
+    DrawRepository drawRepository
+
+    @Autowired
+    PrizeRepository prizeRepository
+
+    @Autowired
+    EventRepository eventRepository
+
+    def setup() {
+        eventRepository.deleteAll()
+        prizeRepository.deleteAll()
+        drawRepository.deleteAll()
+        meetupMemberRepository.deleteAll()
+        memberRepository.deleteAll()
+    }
+}
