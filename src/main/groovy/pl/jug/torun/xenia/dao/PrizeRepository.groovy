e5c9725ae9ca7a28fbe7eff560e4246fd67bbcf7
Zbyszko Papierski <zbyszko.papierski@gmail.com>
2016-05-14 17:20:22 +0200

    merged from upstream, added: - spring boot update - unique price names

--- src/main/groovy/pl/jug/torun/xenia/dao/PrizeRepository.groovy
+++ src/main/groovy/pl/jug/torun/xenia/dao/PrizeRepository.groovy
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository
 import pl.jug.torun.xenia.model.Prize
 
 interface PrizeRepository extends JpaRepository<Prize, String> {
+    long countByName(String name)
+    long countByNameAndIdNot(String name, String id)
 }
